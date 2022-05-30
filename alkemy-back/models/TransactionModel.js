d22da42f1b306e6ce063dda9115af50fa9a380d9
SchleicherConfalonieriN <76877741+SchleicherConfalonieriN@users.noreply.github.com>
2022-05-30 00:08:50 -0300

    Add files via upload

--- alkemy-back/models/TransactionModel.js
+++ alkemy-back/models/TransactionModel.js
@@ -4,13 +4,14 @@ import {DataTypes} from "sequelize";
 
 
 const TransactionModel = db.define('transaction',{
-    Id: {type: DataTypes.INTEGER,
-         primaryKey : true,
-         autoIncremet: true
-    },
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+      },
     Amount: {type: DataTypes.INTEGER},
     Description: {type: DataTypes.STRING},
-    Type: {type: DataTypes.INTEGER},
+    Type: {type: DataTypes.STRING},
     User: {type: DataTypes.INTEGER},
     Date: {type: DataTypes.DATEONLY } ,
     Category: {type: DataTypes.STRING}
--- alkemy-back/models/UserModel.js
+++ alkemy-back/models/UserModel.js
@@ -4,16 +4,19 @@ import {DataTypes} from "sequelize";
 
 
 const UserModel = db.define('users',{
-    Id: {type: DataTypes.INTEGER,
-         primaryKey : true,
-         autoIncremet: true
-    },
-    Email: {type: DataTypes.STRING,
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+      },
+    Email: {type: DataTypes.STRING,   
         allowNull: false,
     },
-    Password: {type: DataTypes.STRING,
-        allowNull: false,}
+    Password: {type: DataTypes.STRING(500),
+        allowNull: false,
+    }
 }
 );
+
 await UserModel.sync(); // if the table dont exist sync  create  table
 export default UserModel;   
