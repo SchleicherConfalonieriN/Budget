74494bdc8b98326dcc2e20ea57315027c22b8378
SchleicherConfalonieriN <76877741+SchleicherConfalonieriN@users.noreply.github.com>
2022-05-30 00:07:59 -0300

    add token managment to transaction routes

--- alkemy-back/routes/TransactionRoutes.js
+++ alkemy-back/routes/TransactionRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express'
-import {createTrasnaction, getAllTransaction,get10Transaction,updateTransaction,deleteTransaction,getTransactionById} from '../controllers/TransactionController.js';
+import {createTrasnaction, getAllTransaction,get10Transaction,updateTransaction,deleteTransaction,getTransactionById, getAllIncome, getAllOutcome} from '../controllers/TransactionController.js';
 const router = express.Router();
 
 
 router.get("/",getAllTransaction);
 router.get("/10",get10Transaction);
+router.get("/Income",getAllIncome);
+router.get("/Outcome",getAllOutcome);
 router.get("/:id",getTransactionById)
 router.post("/",createTrasnaction);
 router.put("/:id",updateTransaction);
